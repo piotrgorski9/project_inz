27ea7af10ee7cf63e0b12ed9a87e5339dc0f9349
Piotr Górski <pgorski4444@gmail.com>
2024-01-03 16:36:10 +0100

    admin panel

--- analysis/admin.py
+++ analysis/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Country, Emigration
 
-# Register your models here.
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('country', 'capital', 'area', 'population', 'phone_code', 'continent', 'flag_path')
+
+class EmigrationAdmin(admin.ModelAdmin):
+    list_display = ('country', 'year', 'number_of_emigrants')
+
+admin.site.register(Country, CountryAdmin)
+admin.site.register(Emigration, EmigrationAdmin)
--- website/settings.py
+++ website/settings.py
@@ -134,3 +134,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.urls import reverse_lazy
+
+LOGOUT_REDIRECT_URL = reverse_lazy('home')
+
